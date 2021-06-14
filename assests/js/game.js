511c37516e65c91986dcb52c7b1c9d2903317d9d
Daniel Zamanillo <dzamanillok@gmail.com>
2021-06-14 16:26:20 -0500

    first js written

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+//fight();
+
+console.log(playerName);
+
+console.log("This logs a string, goof for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+var name = "Dan";
+console.log(name);
